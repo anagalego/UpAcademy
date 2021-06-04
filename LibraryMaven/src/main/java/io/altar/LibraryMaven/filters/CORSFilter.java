11d1cd1e8526f699d435cb5d2d8db3aca08b6da5
Ana Galego <anarita.galego@gmail.com>
2021-06-04 15:31:19 +0100

    no message

---
+++ LibraryMaven/src/main/java/io/altar/LibraryMaven/filters/CORSFilter.java
@@ -0,0 +1,18 @@
+package io.altar.LibraryMaven.filters;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+public class CORSFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
